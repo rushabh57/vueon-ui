ce5ae141f6970bf9fce0441a8b60aa9e54ff2f7e
Rishabh Jain <rushabhmalvaniya12@gmail.com>
2025-10-05 12:46:41 +0530

    structure update

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="space-x-4 p-8">
+    <MyButton>Default</MyButton>
+    <MyButton variant="destructive">Destructive</MyButton>
+    <MyButton variant="outline">Outline</MyButton>
+    <MyButton variant="secondary">Secondary</MyButton>
+    <MyButton variant="ghost">Ghost</MyButton>
+    <MyButton variant="link">Link</MyButton>
+    <MyButton size="sm">Small</MyButton>
+    <MyButton size="lg">Large</MyButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+import MyButton from "./components/Button/Button.vue";
+</script>
